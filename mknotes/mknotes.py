9f090da86840acc6ac7b4f741fd0a0636a06fcd9
Merlin Roe <merlin.roe@hotmail.co.uk>
2020-10-31 17:35:08 +0000

    Base mknotes

---
+++ mknotes/mknotes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import argparse
+import os
+
+
+def parse_args():
+    arguments = argparse.ArgumentParser(
+        description="mknotes. Simple cli tool for creating and managing markdown notes."
+    )
+
+    arguments.add_argument("-a", "--add", dest="add", type=str, help="Add note")
+    arguments.add_argument("-e", "--edit", dest="edit", type=str, help="Edit note")
+    arguments.add_argument(
+        "-d", "--delete", dest="delete", type=str, help="Delete note"
+    )
+
+    args = arguments.parse_args()
+
+    return args
+
+
+def main():
+    arguments = parse_args()
+
+
+if __name__ == "__main__":
+    main()
