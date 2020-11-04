994cb5d1a6137dd1a732aec096831920f7dbc0e4
Merlin Roe <merlin.roe@hotmail.co.uk>
2020-11-04 21:13:44 +0000

    Added barebones add/edit and delete notes

--- mknotes/mknotes.py
+++ mknotes/mknotes.py
@@ -1,20 +1,55 @@
 #!/usr/bin/python3
 import argparse
-import os
+from os import path, listdir, remove
+from pathlib import Path
+from subprocess import call
+from typing import Optional, Dict
 
 from lib.settings import config
 
 
-def parse_args():
+def alter_note(alter_note: Dict, config: Dict):
+    alter_note.alter = path.splitext(alter_note.alter)[0]
+    note_path = path.join(config["notes_location"], "{}.{}".format(alter_note.alter, "md"))
+
+    if path.isfile(note_path) is False:
+        with open(note_path, "w") as note:
+            note.write("#{}".format(alter_note.alter))
+
+    call([config["editor"], note_path])
+
+
+def confirm_choice(msg: Optional[str] = False) -> bool:
+    if msg:
+        print(msg)
+
+    confirm = None 
+    while confirm != 'c' and confirm != 'v':
+        confirm = input("[c]Confirm or [v]Void: ")
+        if confirm != 'c' and confirm != 'v':
+            print("\n Invalid Option. Please Enter a Valid Option.")
+    
+    return True if (confirm == 'c') else False
+
+
+def delete_note(rm_note: Dict, config: Dict):
+    rm_note.delete = path.splitext(rm_note.delete)[0]
+    note_path = path.join(config["notes_location"], "{}.{}".format(rm_note.delete, "md"))
+
+    if path.exists(note_path) is False:
+        print(f"No note called {rm_note.delete}")
+    elif confirm_choice("Do you wish to delete {}".format(rm_note.delete)):
+        remove(note_path)
+
+
+def parse_args() -> Dict:
     arguments = argparse.ArgumentParser(
         description="mknotes. Simple cli tool for creating and managing markdown notes."
     )
 
-    arguments.add_argument("-a", "--add", dest="add", type=str, help="Add note")
-    arguments.add_argument("-e", "--edit", dest="edit", type=str, help="Edit note")
-    arguments.add_argument(
-        "-d", "--delete", dest="delete", type=str, help="Delete note"
-    )
+    arguments.add_argument("-a", "--alter", dest="alter", type=str, help="Add/Edit note")
+    arguments.add_argument("-d", "--delete", dest="delete", type=str, help="Delete note")
+    arguments.add_argument("-l", "--list", dest="list", action='store_true', help="list notes")
 
     args = arguments.parse_args()
 
@@ -23,6 +58,13 @@ def parse_args():
 
 def main():
     arguments = parse_args()
+    
+    if arguments.alter:
+        alter_note(arguments, config)
+    elif arguments.delete:
+        delete_note(arguments, config)
+    elif arguments.list:
+        list_notes(config)
 
 
 if __name__ == "__main__":
