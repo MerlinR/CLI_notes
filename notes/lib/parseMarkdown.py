c24b9afa94bbbd78e2f4d9638a4860594ffe49d0
Merlin Roe <merlin.roe@hotmail.co.uk>
2021-01-08 14:56:05 +0000

    Updated MDV config for set theme

--- notes/lib/parseMarkdown.py
+++ notes/lib/parseMarkdown.py
@@ -11,6 +11,9 @@ from lib.misc import Color, Style, fontColor, fontReset
 class MarkdownParse:
     name = ""
     _notePath = ""
+    # MDV settings
+    _MDV_theme_id = "637.2829"
+    _MDV_headers = "1-"
 
     def __init__(self, note: Note):
         self._note = note
@@ -18,9 +21,6 @@ class MarkdownParse:
             print(f"{note.name} does not exist")
             sys.exit(1)
         self._configure_mdv()
-
-    def _configure_mdv(self):
-        mdv.term_columns = 60
 
     def _read_raw_markdown(self):
         markdown = None
@@ -31,8 +31,18 @@ class MarkdownParse:
     def print(self):
         rows, columns = os.popen("stty size", "r").read().split()
         print(f"{fontColor(style = Style.ITALIC)}{self._note.min_path}{fontReset()}")
+
         raw_markdown = self._read_raw_markdown()
+
+        mdv.term_columns = 60
         if len(raw_markdown) > int(rows):
-            pydoc.pipepager(mdv.main(raw_markdown), cmd="less -R")
+            pydoc.pipepager(
+                mdv.main(
+                    raw_markdown,
+                    theme=self._MDV_theme_id,
+                    header_nrs=self._MDV_headers
+                ),
+                cmd="less -R",
+            )
         else:
             print(mdv.main(raw_markdown))
